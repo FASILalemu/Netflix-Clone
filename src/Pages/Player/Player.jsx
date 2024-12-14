f638a339a77fd3a777f177b76ac8dfc568f1281b
FASILalemu <fasilalemu55@gmail.com>
2024-12-14 10:33:09 +0300

    Create video player page, added functionality of back arrow icon andadded dark layer in the navigation bar

--- src/Pages/Player/Player.jsx
+++ src/Pages/Player/Player.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Player.css"
+import back_arrow_icon from "../../assets/back_arrow_icon.png"
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzkzN2Y3Y2YxOWU5NmY3NWNmYTNkYjFmNmVjNjRkYyIsIm5iZiI6MTczMzc2MDY3Ni43OTUsInN1YiI6IjY3NTcxNmE0OWJjMjBlYWE0OTY4MGQwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._ez-vkgRAzYVe-OpOHj9RILmpyHcMm2gBF6IvNnucP8'
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  },[])
+  
+  
+
+
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}} />
+      <iframe width='90%' height='90%' 
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    
+    </div>
   )
 }
 
